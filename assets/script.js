28fe522e4a0c68d3267388169296e8ca3cb94448
ChunChieh Kwok <ChunChieh Kwok>
2017-08-27 10:20:40 +0800

    增加了第一个应用，产生1~6的随机数。

--- assets/script.js
+++ assets/script.js
@@ -38,4 +38,38 @@ $('.carousel').swipe( {
  });
 
 
+function app1() {
+    console.log("app1");
+
+    //generate random number
+    var a = Math.random() * 10000000000;
+    var b = a.toFixed();
+    var sum = 0;
+    for (var i=0; i<b.length; i++){
+        sum += parseInt(b.charAt(i));
+    }
+    while(sum > 10) {
+        var temp = 0;
+        b = sum.toFixed();
+        for (var i=0; i<b.length; i++){
+            temp += parseInt(b.charAt(i));
+        }
+        sum = temp;
+    }
+    if (sum > 6) {
+        sum = sum % 6;
+    }
+    console.log(sum);
+
+    //init modal
+    $("#modal_title").text("1~6的随机数");
+    $(".modal-body #container").empty();
+    $(".modal-body #container").append("<p class='simple-text'>"+sum+"</p>");
+
+    $('#app_modal').modal({
+        backdrop: "static",
+        keyboard: "true"
+    });
+}
+
 
